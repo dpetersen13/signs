5d27efb1ce9e78f8a8f44ee5c2ad68a0500f1f97
Dylan Petersen <dpetersen023@student.wethinkcode.co.za>
2024-10-08 18:18:47 +0200

    start of translation. idk why it returns none athe the window.

--- translatation.py
+++ translatation.py
@@ -15,26 +15,24 @@ def convert_lang_code(lang_code):
   }
   return lang_mapping.get(lang_code, lang_code)
 
-def translate_from_box():  
+def translate_from_box(input_text, target_lang_code):  
 
+    client = VulavulaClient("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZmODY3MmRhZDllYTQ2N2E4MjU5ZGE3M2VjZDk4MDkyIiwiY2xpZW50X2lkIjoyOTIsInJlcXVlc3RzX3Blcl9taW51dGUiOjAsImxhc3RfcmVxdWVzdF90aW1lIjpudWxsfQ.eU-Jr1DyWcFvgLjvkbbEX0YWtS76Fkw6uwQghhtWwaQ")
 
     # Define the translation data
     translation_data = {
-    "input_text": "begin working now please ." , 
+    "input_text": input_text , 
     "source_lang": ("eng_Latn"),
-    "target_lang": ("zul_Latn")
+    "target_lang": target_lang_code
     }
 
     source_lang = convert_lang_code(translation_data['source_lang'])
     target_lang = convert_lang_code(translation_data['target_lang'])
 
     translation_result = client.translate(translation_data)
-    # spoken_text = translation_result['translation'][0]['translation_text']
     spoken_text = translation_result['translation'][0]['translated_text']
 
-    print(f"Your text, '{translation_data['input_text']}' translated from {source_lang} may be translated into {target_lang} as '{ spoken_text}' ")
-
-translate_from_box()
+    return spoken_text
 
 
 
